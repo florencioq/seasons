1c89dc315f5d568c9b9e02f76166f7ba739926bb
Florencio <florencio@ideos.com.br>
2022-06-22 19:30:10 -0300

    updating

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+    if (month > 3 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    const text = season === 'winter' ? 'Burr, it is chilly' : 'Good';
+    return <div><h1>{text}</h1></div>
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    render() {
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err) 
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMessage: err.message})
         );
-        
-        return <div>Latitude: </div>;
+    }
+
+    componentDidUpdate() {
+        console.log("My component was updated");
+    }
+
+    // Required by React
+    render() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }        
+        return <div>Loading!</div>
     }
 }
 
